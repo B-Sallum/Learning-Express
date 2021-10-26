0c8ce830ab69da86284c9070f89237fe2b4846e7
B-Sallum <brusallum@hotmail.com>
2021-10-26 20:04:29 -0300

    up structure

---
+++ index.js
@@ -0,0 +1,26 @@
+// Server API
+// para inicializar npm i express
+// Importar o Express
+const express = require('express')
+const cors = require('cors')
+
+// Inicializar o Express no arquivo para assumir funções
+const app = express()
+
+// Middleware para utilizar JSON
+app.use(express.json())
+
+// Middleware do Cors para liberar requisições
+app.use(cors())
+
+// Definir a porta onde será executado o servidor BackEnd
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Olá Buuuuuuuu')
+})
+
+// Inicializar o Servidor na porta Pré Definida
+app.listen(port, () => {
+    console.log(`O App está rodando em: http://localhost:${port}`)
+})
