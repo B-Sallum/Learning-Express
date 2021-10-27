8c05c519b7fdca67eac48283eaa7d0027bc27998
B-Sallum <brusallum@hotmail.com>
2021-10-26 22:23:09 -0300

    fununfiando

--- index.js
+++ index.js
@@ -1,35 +1,67 @@
 // Server API
-// para inicializar npm i express
-// Importar o Express
-const express = require('express')
-const cors = require('cors')
+const express = require('express') //Importar o Express (para inicializar npm i express)
+const cors = require('cors') //Importar o cors (para inicializar npm i cors)
 
-// Inicializar o Express no arquivo para assumir funções
-const app = express()
+const app = express()// Inicializar o Express no arquivo para assumir funções
 
-// Middleware para utilizar JSON
-app.use(express.json())
+app.use(express.json()) // Middleware para utilizar JSON
+app.use(cors()) // Middleware do Cors para liberar requisições
 
-// Middleware do Cors para liberar requisições
-app.use(cors())
+const port = 3000 // Definir a porta onde será executado o servidor BackEnd
 
-// Definir a porta onde será executado o servidor BackEnd
-const port = 3000
+const gamesList = [
+
+    {
+        gameName: "Super Mario World",
+        category: "Platform",
+        year: 1990
+    },
+    {
+        gameName: "Zelda: Ocarina of Time",
+        category: "Action RPG",
+        year: 1998
+    }
+
+]
 
 app.get('/', (req, res) => {
-    res.send('Olá Buuuuuuuu')
+    res.send(gamesList)
 })
 
-app.get('/new', (req, res) => {
-    res.send('List')
+app.get('/:id', (req, res) => {
+    const id = req.params.id - 1
+    res.send(gamesList[id])
 })
 
 app.post('/new', (req, res) => {
-    const { nome, idade } = req.body
-    res.send(`Welcome ${nome} idade ${idade}`)
+
+    const { gameName, category, year } = req.body  
+
+    let newGame = { 
+      gameName: gameName,
+      category: category,
+      year: year
+    }
+
+    gamesList.push(newGame)
+
+    console.log('Alguém adicionou um filme novo!')
+    res.send('Thanks for Your Contribution! 💚')
+
 })
+
+// app.put('/list/:id', (req, res) => {
+//   const id = req.params.id
+//   res.send(id)
+// })
+
+// app.delete('list/:id', (req, res) => {
+//   const id = req.params.id -1
+//   delete lista[id]
+//   res.send(lista)
+// })
 
 // Inicializar o Servidor na porta Pré Definida
 app.listen(port, () => {
-    console.log(`O App está rodando em: http://localhost:${port}`)
+  console.log(`O App está rodando em: http://localhost:${port}`)
 })
